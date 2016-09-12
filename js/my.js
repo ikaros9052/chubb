d3e68293b455158241e454f6a7ad838ffd6547ca
1272548043 <1272548043@qq.com>
2016-09-12 14:40:15 +0800

    index

--- js/my.js
+++ js/my.js
@@ -1,4 +1,5 @@
 $(function(){
+  // nav right List
   $(".Open").click(function(){
       if($(".navList_content").css("right")=="0px"){
         $(".navList_content").animate({"right":"8rem"});
@@ -9,13 +10,26 @@ $(function(){
         $(".wrap_sum").css("overflow","auto");
       }
     })
-})
-$(function(){
   $(".navList_head").click(function(){
       $(this).siblings("dd").show();
       $(this).addClass("navList_up");
       $(this).parents("dl").siblings("dl").children("dd").hide();
       $(this).parents("dl").siblings("dl").children("dt").removeClass("navList_up");
     })
+  // nav right List end
+  // dropdown
+  $(".xia-la").click(function(){
+   var $obj=$(this).nextAll("dl");
+   if ($obj.is(":visible")) {
+     $obj.slideUp(300);
+     $(this).children("a").removeClass('momBtnT');
+     $(this).children("a").addClass('momBtn');
+   } else{
+     $obj.slideDown(300);
+      $(this).children("a").removeClass('momBtn');
+     $(this).children("a").addClass('momBtnT');
+    }
+  })
+  // dropdown end
 })
 
